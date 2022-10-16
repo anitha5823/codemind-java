3f5b03167808e6d09ad0a7444cf1a1c7ded96a7a
Ganga Anitha orukurthi <112610582+anitha5823@users.noreply.github.com>
2022-10-16 11:28:26 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.print(a);
+    }
+}
