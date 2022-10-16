7c9579dd996c3bc4f72ee5272441008ff82097dd
Ganga Anitha orukurthi <112610582+anitha5823@users.noreply.github.com>
2022-10-16 11:25:25 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         int a,b,avg;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         avg=a/b;         System.out.print(avg);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        int a,b,avg;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        avg=a/b;
+        System.out.print(avg);
+    }
+}
