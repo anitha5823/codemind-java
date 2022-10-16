75a67b7f3b7ac3c4bf8fbb200a7c84d709b1f9d3
Ganga Anitha orukurthi <112610582+anitha5823@users.noreply.github.com>
2022-10-16 11:19:30 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         char str;         Scanner sc=new Scanner(System.in);         str=sc.next().charAt(0);         int asci=str;         System.out.print(asci);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        char str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.next().charAt(0);
+        int asci=str;
+        System.out.print(asci);
+    }
+}
