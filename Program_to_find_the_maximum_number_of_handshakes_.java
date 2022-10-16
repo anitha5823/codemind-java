8b4c6edca980aa013f9df8221619b3aac1d041fc
Ganga Anitha orukurthi <112610582+anitha5823@users.noreply.github.com>
2022-10-16 11:32:26 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         int a,avg;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         avg=(a*(a-1))/2;         System.out.print(avg);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        int a,avg;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        avg=(a*(a-1))/2;
+        System.out.print(avg);
+    }
+}
