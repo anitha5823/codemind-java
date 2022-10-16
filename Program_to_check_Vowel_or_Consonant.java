cd8307c4bc1f1d92a235ae847e23db96bf685098
Ganga Anitha orukurthi <112610582+anitha5823@users.noreply.github.com>
2022-10-16 13:22:08 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         char a;         Scanner sc=new Scanner(System.in);         a=sc.next().charAt(0);         switch(a)         {             case 'A':System.out.println("Vowel");break;             case 'E':System.out.println("Vowel");break;             case 'I':System.out.println("Vowel");break;             case 'O':System.out.println("Vowel");break;             case 'U':System.out.println("Vowel");break;             case 'a':System.out.println("Vowel");break;             case 'e':System.out.println("Vowel");break;             case 'i':System.out.println("Vowel");break;             case 'o':System.out.println("Vowel");break;             case 'u':System.out.println("Vowel");break;             default:System.out.println("Consonant");         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        char a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.next().charAt(0);
+        switch(a)
+        {
+            case 'A':System.out.println("Vowel");break;
+            case 'E':System.out.println("Vowel");break;
+            case 'I':System.out.println("Vowel");break;
+            case 'O':System.out.println("Vowel");break;
+            case 'U':System.out.println("Vowel");break;
+            case 'a':System.out.println("Vowel");break;
+            case 'e':System.out.println("Vowel");break;
+            case 'i':System.out.println("Vowel");break;
+            case 'o':System.out.println("Vowel");break;
+            case 'u':System.out.println("Vowel");break;
+            default:System.out.println("Consonant");
+        }
+    }
+}
